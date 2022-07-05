c24b414fd5b1567e9dc33639a0b2dfe62221a8a9
Jonathan <kim.jonathan426@gmail.com>
2022-07-05 05:50:19 -0400

    404-page styling

--- frontend/src/components/PageNotFound/index.js
+++ frontend/src/components/PageNotFound/index.js
@@ -1,10 +1,10 @@
+import React from 'react';
+import './PageNotFound.css'
+
 const PageNotFound = () => {
 
     return (
-        <div>
-            <h1>
-                404 Page Not
-            </h1>
+        <div className='page-not-found'>
             <h4>
                 *Whistle* Travel!!!
             </h4>
@@ -12,7 +12,13 @@ const PageNotFound = () => {
                 You traveled to a non-existent page.
                 Avoid this violation by going to a valid path!
             </div>
-            <img src='https://cdn.vox-cdn.com/thumbor/ij0Ng1FcLNz0sjQV6DWRVanCWjc=/0x294:2775x2144/1400x1050/filters:focal(0x294:2775x2144):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/45568342/459394688.0.jpg'></img>
+            <h1>
+                404
+            </h1>
+            <h1>
+                Page Not Found
+            </h1>
+            <img className='travel-image' src='https://cdn.vox-cdn.com/thumbor/ij0Ng1FcLNz0sjQV6DWRVanCWjc=/0x294:2775x2144/1400x1050/filters:focal(0x294:2775x2144):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/45568342/459394688.0.jpg'></img>
         </div>
     )
 }
