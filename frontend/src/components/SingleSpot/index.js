18efeb7c31340d2df307d615a4dd644d5ed04520
Jonathan <kim.jonathan426@gmail.com>
2022-10-10 03:35:41 -0400

    refactored return and added loading state

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -1,43 +1,38 @@
-import { useParams, useHistory } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { getSpots, deleteSpot } from '../../store/spotReducer';
+import { getSingleSpot, deleteSpot } from '../../store/spotReducer';
 import { getReviewAvg } from '../../store/reviewReducer';
 import SpotEditForm from '../SpotEditForm';
 import ReviewForm from '../ReviewForm';
 import SpotReviewList from '../SpotReviewList';
 import ratingStar from '../../images/rating-star.png';
 import './SingleSpot.css';
+import Loading from '../Loading';
 
 const SingleSpot = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
     const history = useHistory();
-    const singleSpot = useSelector(state => state.spot.spots[id]);
+    const spotState = useSelector(state => state.spot.spots);
     const images = useSelector(state => state.spot.images);
     const reviewAvgs = useSelector(state => state.review)
     const user = useSelector(state => state.session?.user?.id);
 
     const [showEditSpotForm, setShowEditSpotForm] = useState(false);
     const [showReviewForm, setShowReviewForm] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const imagesArray = Object.values(images);
+    const spotImages = Object.values(images);
+    const singleSpot = spotState[id];
     const spotAvg = reviewAvgs.reviewAvgs;
-
-    const spotImages = imagesArray.filter(image => image.spotId === +id);
 
     const onDelete = async (e) => {
         e.preventDefault();
 
         const payload = {
             spotId: singleSpot.id,
-            imageIds: [
-                spotImages[0],
-                spotImages[1],
-                spotImages[2],
-                spotImages[3],
-                spotImages[4],
-            ]
+            imageIds: Object.keys(images)
         }
 
         const res = await dispatch(deleteSpot(payload));
@@ -48,39 +43,91 @@ const SingleSpot = () => {
     }
 
     useEffect(() => {
-        dispatch(getSpots());
-        dispatch(getReviewAvg());
-    }, [dispatch])
+        const fetchData = async () => {
+            await dispatch(getSingleSpot(id));
+            await dispatch(getReviewAvg());
 
-    let content = null;
+            setLoading(true);
+        }
 
-    if (!singleSpot) {
-        content = (
-            <div>
-                This page is loading...
-                <br />
-                If the page does not load within 5 seconds, this court has yet to be made, go back!
-            </div>
-        )
-    } else {
-        content = (
-            <>
-                {(!showEditSpotForm && user === singleSpot?.userId) && (
-                    <div className='edit-delete-buttons'>
-                        <button className='edit-button' onClick={() => setShowEditSpotForm(true)}>Edit Court</button>
-                        <button className='delete-button' onClick={onDelete}>Delete Court</button>
+        fetchData();
+    }, [dispatch, id])
+
+
+    return (
+        <div className='single-spot-container'>
+            {loading ? singleSpot ?
+                <>
+                    {(!showEditSpotForm && user === singleSpot?.userId) && (
+                        <div className='edit-delete-buttons'>
+                            <button className='edit-button' onClick={() => setShowEditSpotForm(true)}>Edit Court</button>
+                            <button className='delete-button' onClick={onDelete}>Delete Court</button>
+                        </div>
+                    )}
+                    {(showEditSpotForm && user) && (
+                        <SpotEditForm spot={singleSpot} id={id} hideForm={() => setShowEditSpotForm(false)} />
+                    )}
+                    <div className='spot-header'>
+                        <div className='single-name-info'>
+                            {singleSpot?.name}
+                        </div>
+                        <div className='spot-sub-header'>
+                            <div>
+                                <img className='star-image' src={ratingStar} />
+                                {spotAvg[singleSpot?.id]?.avg && (
+                                    <>
+                                        {' ' + spotAvg[singleSpot?.id]?.avg} | {spotAvg[singleSpot?.id]?.count} reviews
+                                    </>
+                                )}
+                                {!spotAvg[singleSpot?.id]?.avg && (
+                                    <>
+                                        {' New | 0 reviews'}
+                                    </>
+                                )}
+                            </div>
+                            <div className='single-location-info'>
+                                {'| ' + singleSpot?.city}, {singleSpot?.state}, {singleSpot?.country}
+                            </div>
+                        </div>
                     </div>
-                )}
-                {(showEditSpotForm && user) && (
-                    <SpotEditForm spot={singleSpot} id={id} hideForm={() => setShowEditSpotForm(false)} />
-                )}
-                <div className='spot-header'>
-                    <div className='single-name-info'>
-                        {singleSpot?.name}
+                    <div className='single-spot-image-container'>
+                        <div className='main-image'>
+                            <img src={spotImages[0]?.url} className='image-fit-main' />
+                        </div>
+                        <div className='middle-image-top'>
+                            <img src={spotImages[1]?.url} className='image-fit-sub' />
+                        </div>
+                        <div className='middle-image-bottom'>
+                            <img src={spotImages[2]?.url} className='image-fit-sub' />
+                        </div>
+                        <div className='right-image-top'>
+                            <img src={spotImages[3]?.url} className='image-fit-sub right-top' />
+                        </div>
+                        <div className='right-image-bottom'>
+                            <img src={spotImages[4]?.url} className='image-fit-sub right-bot' />
+                        </div>
                     </div>
-                    <div className='spot-sub-header'>
-                        <div>
-                            <img className='star-image' src={ratingStar} />
+                    <div className='single-spot-info'>
+                        <div className='main-spot-content'>
+                            <h3 className='host-name'>
+                                Hosted By {singleSpot?.User?.username}
+                            </h3>
+                            <h5 className='spot-description'>
+                                {singleSpot?.description}
+                            </h5>
+                        </div>
+                        <div className='hovering-content'>
+                            <h5>
+                                Price Per Night
+                            </h5>
+                            <h6>
+                                ${singleSpot?.price}
+                            </h6>
+                        </div>
+                    </div>
+                    <div className='spot-review-container'>
+                        <h3>
+                            <img className='review-star-image' src={ratingStar} />
                             {spotAvg[singleSpot?.id]?.avg && (
                                 <>
                                     {' ' + spotAvg[singleSpot?.id]?.avg} | {spotAvg[singleSpot?.id]?.count} reviews
@@ -91,78 +138,23 @@ const SingleSpot = () => {
                                     {' New | 0 reviews'}
                                 </>
                             )}
-                        </div>
-                        <div className='single-location-info'>
-                            {'| ' + singleSpot?.city}, {singleSpot?.state}, {singleSpot?.country}
-                        </div>
-                    </div>
-                </div>
-                <div className='single-spot-image-container'>
-                    <div className='main-image'>
-                        <img src={spotImages[0]?.url} className='image-fit-main' />
-                    </div>
-                    <div className='middle-image-top'>
-                        <img src={spotImages[1]?.url} className='image-fit-sub' />
-                    </div>
-                    <div className='middle-image-bottom'>
-                        <img src={spotImages[2]?.url} className='image-fit-sub' />
-                    </div>
-                    <div className='right-image-top'>
-                        <img src={spotImages[3]?.url} className='image-fit-sub right-top' />
-                    </div>
-                    <div className='right-image-bottom'>
-                        <img src={spotImages[4]?.url} className='image-fit-sub right-bot' />
-                    </div>
-                </div>
-                <div className='single-spot-info'>
-                    <div className='main-spot-content'>
-                        <h3 className='host-name'>
-                            Hosted By {singleSpot?.User?.username}
                         </h3>
-                        <h5 className='spot-description'>
-                            {singleSpot?.description}
-                        </h5>
-                    </div>
-                    <div className='hovering-content'>
-                        <h5>
-                            Price Per Night
-                        </h5>
-                        <h6>
-                            ${singleSpot?.price}
-                        </h6>
-                    </div>
-                </div>
-                <div className='spot-review-container'>
-                    <h3>
-                        <img className='review-star-image' src={ratingStar} />
-                        {spotAvg[singleSpot?.id]?.avg && (
-                            <>
-                                {' ' + spotAvg[singleSpot?.id]?.avg} | {spotAvg[singleSpot?.id]?.count} reviews
-                            </>
+                        {(!showReviewForm && user) && (
+                            <div className='post-review-button-container'>
+                                <button className='post-review-button' onClick={() => setShowReviewForm(true)}>Post a review</button>
+                            </div>
                         )}
-                        {!spotAvg[singleSpot?.id]?.avg && (
-                            <>
-                                {' New | 0 reviews'}
-                            </>
+                        {(showReviewForm && user) && (
+                            <ReviewForm spotId={id} hideForm={() => setShowReviewForm(false)} />
                         )}
-                    </h3>
-                    {(!showReviewForm && user) && (
-                        <div className='post-review-button-container'>
-                            <button className='post-review-button' onClick={() => setShowReviewForm(true)}>Post a review</button>
-                        </div>
-                    )}
-                    {(showReviewForm && user) && (
-                        <ReviewForm spotId={id} hideForm={() => setShowReviewForm(false)} />
-                    )}
-                    <SpotReviewList user={user} spotId={id} ratingStar={ratingStar} />
-                </div>
-            </>
-        )
-    }
-
-    return (
-        <div className='single-spot-container'>
-            {content}
+                        <SpotReviewList user={user} spotId={id} ratingStar={ratingStar} />
+                    </div>
+                </>
+                :
+                <div>Spot doesn't exist, please return <Link to='/'>Home</Link></div>
+                :
+                <Loading />
+            }
         </div>
     )
 }
