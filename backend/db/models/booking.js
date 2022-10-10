fd34345f376b045c4b67ea0b6f20e1aad834b1a4
Jonathan <kim.jonathan426@gmail.com>
2022-10-10 16:22:13 -0400

    added bookings model, migration, and route skeleton

---
+++ backend/db/models/booking.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Booking = sequelize.define('Booking', {
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    spotId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    startDate: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    endDate: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {});
+  Booking.associate = function(models) {
+    Booking.belongsTo(models.User, {
+      foreignKey: 'userId'
+    })
+    Booking.belongsTo(models.Spot, {
+      foreignKey: 'spotId'
+    })
+  };
+  return Booking;
+};
