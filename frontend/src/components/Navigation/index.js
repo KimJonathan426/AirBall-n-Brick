d785f7fc690b6ffba72448030947808bfb27d591
Jonathan <kim.jonathan426@gmail.com>
2022-10-27 20:51:23 -0400

    cleaned up code and unused css as a result of dropdown implementation

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import DemoButton from '../DemoButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
+import NavDropdown from '../NavDropdown';
 import logoImage from '../../images/logo-image.png';
 import logoText from '../../images/logo-text.png';
 import './Navigation.css';
@@ -32,23 +29,14 @@ function Navigation({ isLoaded }) {
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <div className='session-logged-in'>
-        <div className='about-link-logged-in-container'>
-          <NavLink className='about-link-logged-in' exact to='/about'>About</NavLink>
-        </div>
-        <div className='host-link-container'>
-          <NavLink className='host-link' to="/spots/new">Host a Court</NavLink>
-        </div>
-        <ProfileButton user={sessionUser} />
-      </div>
+      <>
+        <NavDropdown sessionUser={sessionUser} />
+      </>
     );
   } else {
     sessionLinks = (
       <>
-        <NavLink className='about-link-logged-out' exact to='/about'>About</NavLink>
-        <DemoButton />
-        <LoginFormModal />
-        <SignupFormModal />
+        <NavDropdown sessionUser={sessionUser} />
       </>
     );
   }
@@ -59,7 +47,7 @@ function Navigation({ isLoaded }) {
         <img className='logo-image' src={logoImage} />
         <img className='logo-text' src={logoText} />
       </NavLink>
-      <div className='nav-buttons'>
+      <div>
         {isLoaded && sessionLinks}
       </div>
     </div>
