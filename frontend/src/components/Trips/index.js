6184b7b021066f58682222a125112f2dc7279e04
Jonathan <kim.jonathan426@gmail.com>
2022-11-03 03:20:24 -0400

    finished screen responsive "where youve been" section

--- frontend/src/components/Trips/index.js
+++ frontend/src/components/Trips/index.js
@@ -30,8 +30,30 @@ const Trips = () => {
 
 
                 for (let trip of trips) {
+                    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+                    const startDate = new Date(trip.startDate);
                     const endDate = new Date(trip.endDate);
                     endDate.setTime(endDate.getTime() + 23.9999 * 60 * 60 * 1000);
+
+                    const startDateDay = startDate.getDate();
+                    const startDateMonth = months[startDate.getMonth()];
+                    const startDateYear = startDate.getFullYear();
+                    const endDateDay = endDate.getDate();
+                    const endDateMonth = months[endDate.getMonth()];
+                    const endDateYear = endDate.getFullYear();
+
+                    let date = `${startDateMonth} ${startDateDay}–${endDateDay}, ${endDateYear}`;
+
+                    if (startDateYear !== endDateYear) {
+                        date = `${startDateMonth} ${startDateDay}, ${startDateYear} – ${endDateMonth} ${endDateDay}, ${endDateYear}`;
+                    } else if (startDateMonth !== endDateMonth) {
+                        date = `${startDateMonth} ${startDateDay}–${endDateMonth} ${endDateDay}, ${endDateYear}`;
+                    } else if (startDateDay === endDateDay) {
+                        date = `${startDateMonth} ${startDateDay}`;
+                    }
+
+                    trip['date'] = date;
 
                     if (currentDate > endDate) past.push(trip);
                     else active.push(trip);
@@ -79,19 +101,49 @@ const Trips = () => {
                             <div className='no-booked-trips-image-box'>
                                 <div className='trips-picture-box'>
                                     <picture>
-                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-320.webp 1x" media="(max-width: 743px)"/>
-                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-320.webp 1x" media="(min-width: 743.1px) and (max-width: 1127px)"/>
-                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-720.webp 1x" media="(min-width: 1127.1px) and (max-width: 1439px)"/>
-                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-1200.webp 1x" media="(min-width: 1439.1px)"/>
-                                        <img className='main-picture' src="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-720.webp"/>
+                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-320.webp 1x" media="(max-width: 743px)" />
+                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-320.webp 1x" media="(min-width: 743.1px) and (max-width: 1127px)" />
+                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-720.webp 1x" media="(min-width: 1127.1px) and (max-width: 1439px)" />
+                                        <source srcSet="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-1200.webp 1x" media="(min-width: 1439.1px)" />
+                                        <img className='main-picture' src="https://airballnbrick.s3.amazonaws.com/trips-airbnb-background-720.webp" />
                                     </picture>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div>
-                        Where you've been
-                    </div>
+                    {pastTrips &&
+                        <>
+                            <h1 className='previous-trips-header'>Where you've been</h1>
+                            <div className='previous-trips-container'>
+                                <ul className='previous-trips-list'>
+                                    {pastTrips.map(trip => (
+                                        <li key={trip.id} className='previous-trips-item'>
+                                            <div className='previous-trips-image-box'>
+                                                <img className='previous-trips-image' src={trip.url} />
+                                            </div>
+                                            <div className='previous-trips-info-box'>
+                                                <div className='previous-trips-info-1'>
+                                                    <span>
+                                                        {trip.Spot.name}
+                                                    </span>
+                                                </div>
+                                                <div className='previous-trips-info-2'>
+                                                    <span>
+                                                        Hosted By {trip.User.username}
+                                                    </span>
+                                                </div>
+                                                <div className='previous-trips-info-2'>
+                                                    <span>
+                                                        {trip.date}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
             :
