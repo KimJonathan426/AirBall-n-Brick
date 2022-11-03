af949cc467b0ca292a23bfbfe6f28ba0bc6f4875
Jonathan <kim.jonathan426@gmail.com>
2022-11-03 03:16:20 -0400

    added additional booking seeders for previous dates

--- backend/db/seeders/20221010204158-bookingSeed.js
+++ backend/db/seeders/20221010204158-bookingSeed.js
@@ -12,12 +12,34 @@ module.exports = {
     futureDate.setDate(futureDate.getDate() + 3);
 
    return queryInterface.bulkInsert('Bookings', [{
-     userId: 1,
+     userId: 4,
      spotId: 1,
      startDate: new Date(),
      endDate: futureDate,
      url: 'https://airballnbrick.s3.amazonaws.com/Cavs+Arena.jpg'
-   }], {});
+   },
+   {
+    userId: 4,
+    spotId: 1,
+    startDate: new Date('2020-12-17T00:00:00'),
+    endDate: new Date('2020-12-18T00:00:00'),
+    url: 'https://airballnbrick.s3.amazonaws.com/Cavs+Arena.jpg'
+   },
+   {
+    userId: 4,
+    spotId: 11,
+    startDate: new Date('2022-08-02T00:00:00'),
+    endDate: new Date('2022-08-10T00:00:00'),
+    url: "https://airballnbrick.s3.amazonaws.com/Jordan's+Yacht+1.jpg"
+   },
+   {
+    userId: 4,
+    spotId: 15,
+    startDate: new Date('2022-10-21T00:00:00'),
+    endDate: new Date('2022-11-01T00:00:00'),
+    url: "https://airballnbrick.s3.amazonaws.com/OVO+1.jpg"
+   },
+  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
