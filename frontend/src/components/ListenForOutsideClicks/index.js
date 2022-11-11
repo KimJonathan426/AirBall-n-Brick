34b79faccf4acd8bd2fd711660dcd824dd735101
Jonathan <kim.jonathan426@gmail.com>
2022-11-11 00:12:54 -0500

    added function that listens for clicks outside of reference element passed in

---
+++ frontend/src/components/ListenForOutsideClicks/index.js
@@ -0,0 +1,20 @@
+export default function listenForOutsideClicks(
+  listening,
+  setListening,
+  bookingRef,
+  setIsOpen
+) {
+  return () => {
+    if (listening) return;
+    if (!bookingRef.current) return;
+    setListening(true);
+    [`click`, `touchstart`].forEach((type) => {
+      document.addEventListener(`click`, (evt) => {
+        const cur = bookingRef.current;
+        const node = evt.target;
+        if (cur.contains(node)) return;
+        setIsOpen(false);
+      });
+    });
+  };
+}
