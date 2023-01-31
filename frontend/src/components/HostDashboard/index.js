619b8457dd280e4a609ac78f3d73bfb5d5e82905
Jonathan <kim.jonathan426@gmail.com>
2023-01-30 21:50:43 -0500

    hosting dashboard static displays finished;

--- frontend/src/components/HostDashboard/index.js
+++ frontend/src/components/HostDashboard/index.js
@@ -16,8 +16,36 @@ const HostDashboard = () => {
                 <div className='hosting-reservations'>
 
                 </div>
-                <div className='hosting-tips'>
-
+                <div className='hosting-tips-container'>
+                    <div className='tips-header'>
+                        Hosting Tips
+                    </div>
+                    <ul className='tips-list'>
+                        <li className='tip-box'>
+                            <div className='tip-image'>
+                                <img src='https://airballnbrick.s3.amazonaws.com/Luxury-court-landscape.jpg' alt='exotic court'/>
+                            </div>
+                            <div className='tip-info'>Make your court stand out</div>
+                        </li>
+                        <li className='tip-box'>
+                            <div className='tip-image'>
+                                <img src='https://airballnbrick.s3.amazonaws.com/basketball-dunk-landscape.PNG' alt='mid dunk poster' />
+                            </div>
+                            <div className='tip-info'>Show great photos of your court</div>
+                        </li>
+                        <li className='tip-box'>
+                            <div className='tip-image'>
+                                <img src='https://airballnbrick.s3.amazonaws.com/warm-court-landscape.jpg' alt='warm basketball court' />
+                            </div>
+                            <div className='tip-info'>Write a great listing description</div>
+                        </li>
+                        <li className='tip-box'>
+                            <div className='tip-image'>
+                                <img src='https://airballnbrick.s3.amazonaws.com/basketball-group-landscape.jpg' alt='players talking' />
+                            </div>
+                            <div className='tip-info'>Create a great experience for your customers</div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
