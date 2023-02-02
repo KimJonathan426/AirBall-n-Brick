fe93d794c1b1dd73210ac762e76551a5a7e9bf40
Jonathan <kim.jonathan426@gmail.com>
2023-02-01 22:24:34 -0500

    redesigned email input and added responsive css to invalid input

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
-import authExit from '../../images/auth-exit.svg'
+import authExit from '../../images/auth-exit.svg';
 import './LoginForm.css'
 
 
@@ -25,7 +25,7 @@ function LoginForm({ setShowModal }) {
   console.log(errors)
 
   return (
-    <form className='login-form animate-modal-auth' onSubmit={handleSubmit}>
+    <div className='login-form animate-modal-auth'>
       <header className='auth-header'>
         <button className='auth-exit' onClick={() => setShowModal(false)}>
           <img src={authExit} />
@@ -34,37 +34,43 @@ function LoginForm({ setShowModal }) {
           Log in or sign up
         </div>
       </header>
-      <div className='errors-container'>
+      <div className='auth-content'>
+        <h3 className='auth-welcome'>
+          Welcome to AirBallnBrick
+        </h3>
+        <form className='auth-form' onSubmit={handleSubmit}>
+          {/* <div className='errors-container'>
         <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
           ))}
-        </ul>
+          </ul>
+        </div> */}
+          <div className='credential-container'>
+            <input
+              type="email"
+              pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"              className='credential-email'
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+            <div className='credential-header'></div>
+          </div>
+          {/* <div className='password-container'>
+            <label>
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div> */}
+          {/* <button className='login-form-button' type="submit">Continue</button> */}
+        </form>
       </div>
-      <div className='credential-container'>
-        <label>
-          Email
-        </label>
-        <input
-          type="email"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </div>
-      <div className='password-container'>
-        <label>
-          Password
-        </label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button className='login-form-button' type="submit">Continue</button>
-    </form>
+    </div>
   );
 }
 
