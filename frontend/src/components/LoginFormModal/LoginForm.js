fafbc97bcf08ef0fa0cae1b6fdcad6b8deaa0b90
Jonathan <kim.jonathan426@gmail.com>
2023-02-09 17:03:39 -0500

    changed password input styling to match new design and added loading element on button click

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import * as sessionActions from "../../store/session";
 import { findEmail } from "../../store/session";
 import { useDispatch } from "react-redux";
+import loadingGif from '../../images/host-court-loading.gif';
 import authExit from '../../images/auth-exit.svg';
 import errorMark from '../../images/error-mark.png';
 import './LoginForm.css'
@@ -11,8 +12,10 @@ function LoginForm({ setShowModal }) {
   const dispatch = useDispatch();
   const [credential, setCredential] = useState("");
   const [password, setPassword] = useState("");
+  const [uploading, setUploading] = useState(false);
   const [errors, setErrors] = useState([]);
   const [emptyEmail, setEmptyEmail] = useState(true);
+  const [emptyPassword, setEmptyPassword] = useState(true);
   const [stepOne, setStepOne] = useState(true);
   const [stepOneErrors, setStepOneErrors] = useState([]);
   const [login, setLogin] = useState(false);
@@ -28,6 +31,16 @@ function LoginForm({ setShowModal }) {
     setStepOneErrors([])
   }, [credential, setEmptyEmail])
 
+  useEffect(() => {
+    if (password === "") {
+      setEmptyPassword(true);
+    } else {
+      setEmptyPassword(false);
+    }
+
+    setErrors([])
+  }, [password, setEmptyPassword])
+
   const nextStep = async (e) => {
     e.preventDefault();
 
@@ -37,28 +50,43 @@ function LoginForm({ setShowModal }) {
       return
     }
 
+    setUploading(true);
+
     const res = await dispatch(findEmail(credential)).catch(
       async (res) => {
         const data = await res.json();
-        if (data && data.errors) setStepOneErrors(data.errors);
+        if (data && data.errors) {
+          setStepOneErrors(data.errors);
+          setUploading(false);
+        }
       });
 
     if (stepOneErrors.length) {
+      setUploading(false);
       return
     } else if (res && res.result) {
+      setStepOne(false);
       setLogin(true);
+      setUploading(false);
     } else if (res && !res.result) {
+      setStepOne(false);
       setSignup(true);
+      setUploading(false);
     }
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setUploading(true);
     setErrors([]);
+
     return dispatch(sessionActions.login({ credential, password })).catch(
       async (res) => {
         const data = await res.json();
-        if (data && data.errors) setErrors(data.errors);
+        if (data && data.errors) {
+          setErrors(data.errors);
+          setUploading(false);
+        }
       }
     );
   };
@@ -79,39 +107,69 @@ function LoginForm({ setShowModal }) {
           Welcome to AirBallnBrick
         </h3>
         <form className='auth-form' onSubmit={handleSubmit}>
-          <div className='credential-container'>
-            <input
-              type="email"
-              pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,4}|[0-9]{1,3})(\]?)$"
-              className={emptyEmail ? 'credential-email' : 'credential-email invalid-email'}
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              required
-            />
-            <div className='credential-header'></div>
-          </div>
-          {stepOneErrors.length > 0 && (
-          <div className='auth-error-container'>
-            <ul className='auth-error-list'>
-              {stepOneErrors.map((error, idx) => (
-                  <li className='auth-error-item' key={idx}><img className='exclamation-mark' src={errorMark} />{error}</li>
-              ))}
-            </ul>
-          </div>
+          {stepOne && (
+            <div className='credential-container'>
+              <input
+                type="email"
+                pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,4}|[0-9]{1,3})(\]?)$"
+                className={emptyEmail ? 'credential' : 'credential invalid'}
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+              <div className='credential-header credential-email'></div>
+            </div>
           )}
-          <button className='auth-next' onClick={nextStep}>Continue</button>
-          {/* <div className='password-container'>
-            <label>
-              Password
-            </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div> */}
-          {/* <button className='login-form-button' type="submit">Continue</button> */}
+          {stepOneErrors.length > 0 && (
+            <div className='auth-error-container'>
+              <ul className='auth-error-list'>
+                {stepOneErrors.map((error, idx) => (
+                  <li className='auth-error-item' key={idx}><img className='exclamation-mark' src={errorMark} />{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {stepOne &&
+            <>
+              {uploading ?
+                <button disabled className='credential-loading'>
+                  <img className='credential-loading-img' src={loadingGif} alt='loading...' />
+                </button>
+                :
+                <button className='auth-next' onClick={nextStep}>Continue</button>
+              }
+            </>
+          }
+          {login && (
+            <>
+              <div className='credential-container'>
+                <input
+                  type="password"
+                  className={emptyPassword ? 'credential' : 'credential invalid'}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                <div className='credential-header credential-password'></div>
+              </div>
+              {errors.length > 0 && (
+                <div className='auth-error-container'>
+                  <ul className='auth-error-list'>
+                    {errors.map((error, idx) => (
+                      <li className='auth-error-item' key={idx}><img className='exclamation-mark' src={errorMark} />{error}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {uploading ?
+                <button disabled className='credential-loading'>
+                  <img className='credential-loading-img' src={loadingGif} alt='loading...' />
+                </button>
+                :
+                <button className='auth-next' type='submit'>Continue</button>
+              }
+            </>
+          )}
         </form>
       </div>
     </div>
