50f9e368d13cab41f2636eb2998c2135ad59eba2
Jonathan <kim.jonathan426@gmail.com>
2023-03-16 12:50:18 -0400

    created function to get google oauth url

---
+++ backend/utils/getGoogleUrls.js
@@ -0,0 +1,26 @@
+const getGoogleOAuthURL = () => {
+    const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    const options = {
+        redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        access_type: 'offline',
+        response_type: 'code',
+        prompt: 'consent',
+        scopes: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+        ].join(' '),
+    };
+
+    console.log({ options })
+
+    const qs = new URLSearchParams(options);
+
+    console.log({ qs })
+
+    return `${rootUrl}?${qs.toString()}`;
+
+}
+
+export default getGoogleOAuthURL
