4e5f11021c77b0f9381926e33d18b0f83fd47d39
Jonathan <kim.jonathan426@gmail.com>
2023-03-16 16:46:00 -0400

    moved function to frontend where root application lies

--- frontend/src/utils/getGoogleUrl.js
+++ frontend/src/utils/getGoogleUrl.js
@@ -2,25 +2,20 @@ const getGoogleOAuthURL = () => {
     const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
 
     const options = {
-        redirect_uri: process.env.GOOGLE_OAUTH_REDIRECT_URL,
-        client_id: process.env.GOOGLE_CLIENT_ID,
+        redirect_uri: process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL,
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
         access_type: 'offline',
         response_type: 'code',
         prompt: 'consent',
-        scopes: [
+        scope: [
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/userinfo.email',
         ].join(' '),
     };
 
-    console.log({ options })
-
     const qs = new URLSearchParams(options);
 
-    console.log({ qs })
-
     return `${rootUrl}?${qs.toString()}`;
-
 }
 
 export default getGoogleOAuthURL
