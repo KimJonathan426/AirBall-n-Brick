9442fa834cdaf93fcc2f0364ab41d696c1363c14
Jonathan <kim.jonathan426@gmail.com>
2023-05-02 17:28:17 -0400

    changed list to dynamic slider

--- frontend/src/components/HostDashboard/index.js
+++ frontend/src/components/HostDashboard/index.js
@@ -1,3 +1,7 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 import './HostDashboard.css';
 
 const HostDashboard = () => {
@@ -20,32 +24,48 @@ const HostDashboard = () => {
                     <div className='tips-header'>
                         Hosting Tips
                     </div>
-                    <ul className='tips-list'>
-                        <li className='tip-box'>
-                            <div className='tip-image'>
-                                <img src='https://airballnbrick.s3.amazonaws.com/Luxury-court-landscape.jpg' alt='exotic court'/>
-                            </div>
-                            <div className='tip-info'>Make your court stand out</div>
-                        </li>
-                        <li className='tip-box'>
-                            <div className='tip-image'>
-                                <img src='https://airballnbrick.s3.amazonaws.com/basketball-dunk-landscape.PNG' alt='mid dunk poster' />
-                            </div>
-                            <div className='tip-info'>Show great photos of your court</div>
-                        </li>
-                        <li className='tip-box'>
-                            <div className='tip-image'>
-                                <img src='https://airballnbrick.s3.amazonaws.com/warm-court-landscape.jpg' alt='warm basketball court' />
-                            </div>
-                            <div className='tip-info'>Write a great listing description</div>
-                        </li>
-                        <li className='tip-box'>
-                            <div className='tip-image'>
-                                <img src='https://airballnbrick.s3.amazonaws.com/basketball-group-landscape.jpg' alt='players talking' />
-                            </div>
-                            <div className='tip-info'>Create a great experience for your customers</div>
-                        </li>
-                    </ul>
+                    <Swiper
+                        slidesPerView={3}
+                        spaceBetween={16}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination]}
+                        className="tips-list"
+                    >
+                        <SwiperSlide>
+                            <li className='tip-box'>
+                                <div className='tip-image'>
+                                    <img src='https://airballnbrick.s3.amazonaws.com/Luxury-court-landscape.jpg' alt='exotic court' />
+                                    <div className='tip-info'>Make your court stand out</div>
+                                </div>
+                            </li>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <li className='tip-box'>
+                                <div className='tip-image'>
+                                    <img src='https://airballnbrick.s3.amazonaws.com/basketball-dunk-landscape.PNG' alt='mid dunk poster' />
+                                </div>
+                                <div className='tip-info'>Show great photos of your court</div>
+                            </li>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <li className='tip-box'>
+                                <div className='tip-image'>
+                                    <img src='https://airballnbrick.s3.amazonaws.com/warm-court-landscape.jpg' alt='warm basketball court' />
+                                </div>
+                                <div className='tip-info'>Write a great listing description</div>
+                            </li>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <li className='tip-box'>
+                                <div className='tip-image'>
+                                    <img src='https://airballnbrick.s3.amazonaws.com/basketball-group-landscape.jpg' alt='players talking' />
+                                </div>
+                                <div className='tip-info'>Create a great experience for your customers</div>
+                            </li>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </div>
