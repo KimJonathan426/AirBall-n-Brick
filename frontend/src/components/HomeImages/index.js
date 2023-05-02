fb8de4958a17b4f53d616e8fa416241c8b9b3e0a
Jonathan <kim.jonathan426@gmail.com>
2023-05-02 18:33:09 -0400

    updated manual slider with custom swiper

--- frontend/src/components/HomeImages/index.js
+++ frontend/src/components/HomeImages/index.js
@@ -1,124 +1,43 @@
-import { useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const HomeImages = ({ images }) => {
 
-    const [showImage1, setShowImage1] = useState(true);
-    const [showImage2, setShowImage2] = useState(false);
-    const [showImage3, setShowImage3] = useState(false);
-    const [showImage4, setShowImage4] = useState(false);
-    const [showImage5, setShowImage5] = useState(false);
-
-    const switchImageBackward = (e) => {
-        e.preventDefault();
-
-        switch (true) {
-            case showImage1:
-                setShowImage1(false);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(true);
-                break;
-            case showImage2:
-                setShowImage1(true);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-            case showImage3:
-                setShowImage1(false);
-                setShowImage2(true);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-            case showImage4:
-                setShowImage1(false);
-                setShowImage2(false);
-                setShowImage3(true);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-            case showImage5:
-                setShowImage1(false);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(true);
-                setShowImage5(false);
-                break;
-            default:
-                setShowImage1(true);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-        }
-    }
-
-    const switchImageForward = (e) => {
-        e.preventDefault();
-
-        switch (true) {
-            case showImage1:
-                setShowImage1(false);
-                setShowImage2(true);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-            case showImage2:
-                setShowImage1(false);
-                setShowImage2(false);
-                setShowImage3(true);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-            case showImage3:
-                setShowImage1(false);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(true);
-                setShowImage5(false);
-                break;
-            case showImage4:
-                setShowImage1(false);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(true);
-                break;
-            case showImage5:
-                setShowImage1(true);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-            default:
-                setShowImage1(true);
-                setShowImage2(false);
-                setShowImage3(false);
-                setShowImage4(false);
-                setShowImage5(false);
-                break;
-        }
-    }
-
-
     return (
-        <div className='spot-image'>
-            <img className={showImage1 ? 'center' : 'center hide-image'} src={images[0].url} alt='listing preview' />
-            <img className={showImage2 ? 'center' : 'center hide-image'} src={images[1].url} alt='listing preview' />
-            <img className={showImage3 ? 'center' : 'center hide-image'} src={images[2].url} alt='listing preview' />
-            <img className={showImage4 ? 'center' : 'center hide-image'} src={images[3].url} alt='listing preview' />
-            <img className={showImage5 ? 'center' : 'center hide-image'} src={images[4].url} alt='listing preview' />
+        <Swiper
+            cssMode={true}
+            navigation={{
+                prevEl: '.left-arrow',
+                nextEl: '.right-arrow',
+            }}
+            pagination={true}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+            className="spot-image"
+        >
+            <SwiperSlide className='preview-slide'>
+                <img className='center' src={images[0].url} alt='listing preview' />
+            </SwiperSlide>
+            <SwiperSlide className='preview-slide'>
+                <img className='center' src={images[1].url} alt='listing preview' />
+            </SwiperSlide>
+            <SwiperSlide className='preview-slide'>
+                <img className='center' src={images[2].url} alt='listing preview' />
+            </SwiperSlide>
+            <SwiperSlide className='preview-slide'>
+                <img className='center' src={images[3].url} alt='listing preview' />
+            </SwiperSlide>
+            <SwiperSlide className='preview-slide'>
+                <img className='center' src={images[4].url} alt='listing preview' />
+            </SwiperSlide>
             <div className='image-switch-btns'>
-                <button onClick={switchImageBackward} className='left-arrow'><span>❮</span></button>
-                <button onClick={switchImageForward} className='right-arrow'><span>❯</span></button>
+                <button className='left-arrow'><span>❮</span></button>
+                <button className='right-arrow'><span>❯</span></button>
             </div>
-        </div>
+        </Swiper>
     )
 }
 
