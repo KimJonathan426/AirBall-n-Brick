f190c337068f5e26eaac6569dcea83f5da622928
Jonathan <kim.jonathan426@gmail.com>
2023-05-04 02:35:59 -0400

    successful dispatch and state representation of current user hosted spots

--- frontend/src/components/HostDashboard/HostingReservations.js
+++ frontend/src/components/HostDashboard/HostingReservations.js
@@ -1,7 +1,28 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getUserSpots } from '../../store/spotReducer';
 
 const HostingReservations = () => {
+    const dispatch = useDispatch();
+    const spots = useSelector(state => state.spot)
+    const user = useSelector(state => state.session.user)
+    const [checkingOut, setCheckingOut] = useState([]);
+    const [currentlyHosting, setCurrentlyHosting] = useState([]);
+    const [arrivingSoon, setArrivingSoon] = useState([]);
+    const [upcoming, setUpcoming] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await dispatch(getUserSpots(user.id));
+
+            setLoading(true);
+        }
+
+        fetchData();
+    }, [dispatch]);
+
+
     return (
         <div className='hosting-reservations-container'>
             <div className='hosting-reservations-main'>
