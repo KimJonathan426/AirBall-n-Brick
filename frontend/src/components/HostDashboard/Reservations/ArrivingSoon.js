d044a39927624c926103436c51580eb37fea28a8
Jonathan <kim.jonathan426@gmail.com>
2023-05-09 01:39:09 -0400

    finished hosting dashboard with current features

--- frontend/src/components/HostDashboard/Reservations/ArrivingSoon.js
+++ frontend/src/components/HostDashboard/Reservations/ArrivingSoon.js
@@ -1,10 +1,33 @@
+import moment from 'moment';
 import reservationIcon from '../../../images/reservation-icon.svg';
 
 const ArrivingSoon = ({ reservations }) => {
     return (
         reservations.length > 0 ?
-            <div>
-                Yes
+            <div className='reservation-display'>
+                {reservations.map((reservation) => (
+                    <div key={reservation.id} className='hosted-reservation'>
+                        <div className='hosted-reservation-container'>
+                            <div className='hosted-reservation-info'>
+                                <div className='hosted-reservation-info-inner'>
+                                    <strong>Check-in</strong>
+                                    <span>{moment.utc(reservation.startDate).format('MMMM DD, YYYY')}</span>
+                                </div>
+                                <div className='hosted-reservation-info-inner'>
+                                    <strong>Check-out</strong>
+                                    <span>{moment.utc(reservation.endDate).format('MMMM DD, YYYY')}</span>
+                                </div>
+                            </div>
+                            <div className='hosted-reservation-image-container'>
+                                <img className='hosted-reservation-image' src={reservation.url} alt='reservation spot' />
+                            </div>
+                        </div>
+                        <div className='hosted-reservation-footer'>
+                            <strong>Guest </strong>
+                            <span className='hosted-guest-name'>{reservation.User.username}</span>
+                        </div>
+                    </div>
+                ))}
             </div>
             :
             <div className='reservation-empty-container'>
