8f8aaf52de926a3e10a31aa873c22effb6948e58
Jonathan <kim.jonathan426@gmail.com>
2023-06-13 05:01:59 -0400

    fixed error with code attempting to login using encrypted email and updated component to match changes with other google components

--- frontend/src/components/GoogleOAuth/GoogleOAuthLogin.js
+++ frontend/src/components/GoogleOAuth/GoogleOAuthLogin.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import CryptoJS from "crypto-js";
 import validator from 'validator';
 import * as sessionActions from "../../store/session";
 import loadingGif from '../../images/host-court-loading.gif';
 import GeneralLoginError from "../GeneralLoginError";
-import PageNotFound from "../PageNotFound";
 import Loading from "../Loading";
 import './GoogleOAuth.css';
 
 const GoogleOAuthLogin = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const sessionUser = useSelector((state) => state.session.user);
   const { ivString } = useParams();
   const token = useParams()['*'];
-  const [email, setEmail] = useState(token);
+  const [email, setEmail] = useState(null);
   const [errors, setErrors] = useState([]);
-  const [isValidPopup, setIsValidPopup] = useState(false);
   const [loading, setLoading] = useState(false);
 
   // validate user came from backend api with encryption/decryption and window name
@@ -36,14 +35,17 @@ const GoogleOAuthLogin = () => {
 
     setEmail(decryptedEmail);
 
-    if (window.name === 'airballnbrick_google_popup' && validator.isEmail(decryptedEmail)) {
-      setIsValidPopup(true);
+    if (window.name !== 'airballnbrick_google_popup' || !validator.isEmail(decryptedEmail)) {
+      navigate('/oauth/error');
     };
 
     setLoading(true);
 
+  }, [navigate, ivString, token]);
+
+  useEffect(() => {
     const loginPage = async () => {
-      if (isValidPopup) {
+      if (email) {
         const response = await dispatch(sessionActions.googleLogin({ email })).catch(
           async (res) => {
             const data = await res.json();
@@ -58,16 +60,17 @@ const GoogleOAuthLogin = () => {
           window.opener.focus();
           window.close();
         };
-      };
+      }
     };
 
     loginPage();
-  }, [dispatch, isValidPopup, email, ivString, token]);
+  }, [dispatch, email, loading])
 
   if (sessionUser) return <Navigate replace to="/" />;
 
+
   return (
-    loading ? isValidPopup ?
+    loading ?
       <div>
         {errors.length > 0 ? (
           <GeneralLoginError />
@@ -79,8 +82,6 @@ const GoogleOAuthLogin = () => {
           </div>
         }
       </div>
-      :
-      <PageNotFound />
       :
       <Loading />
   )
