6e2279167534c24aaa9a70778b42f83afefaa89b
Jonathan <kim.jonathan426@gmail.com>
2023-06-26 19:19:57 -0400

    finished back and next buttons

--- frontend/src/components/HostSpot/HostFooter.js
+++ frontend/src/components/HostSpot/HostFooter.js
@@ -8,7 +8,14 @@ const HostFooter = ({ step, setStep }) => {
             {/* <div className='host-spot-start'>
                 <button onClick={() => setStep(step + 1)} className='host-spot-start-btn'>Get Started</button>
             </div> */}
-            <div className='host-spot-step'>
+
+            <div className='host-spot-footer-progress'>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='host-spot-footer-step'>
+                <button onClick={() => setStep(step - 1)} className='host-spot-back-btn'>Back</button>
                 <button onClick={() => setStep(step + 1)} className='host-spot-next-btn'>Next</button>
             </div>
         </>
