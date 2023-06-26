a1d88f9010622840318efec963b5a641573c233a
Jonathan <kim.jonathan426@gmail.com>
2023-06-25 23:24:11 -0400

    started alternate footer for steps

--- frontend/src/components/HostSpot/HostFooter.js
+++ frontend/src/components/HostSpot/HostFooter.js
@@ -5,8 +5,11 @@ const HostFooter = ({ step, setStep }) => {
 
     return (
         <>
-            <div className='host-spot-start'>
-                <button onClick={() => setStep(step + 1)}className='host-spot-start-btn'>Get Started</button>
+            {/* <div className='host-spot-start'>
+                <button onClick={() => setStep(step + 1)} className='host-spot-start-btn'>Get Started</button>
+            </div> */}
+            <div className='host-spot-step'>
+                <button onClick={() => setStep(step + 1)} className='host-spot-next-btn'>Next</button>
             </div>
         </>
     )
