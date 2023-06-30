34af1727a99e8f924dfe9e20462383d2fc00bee3
Jonathan <kim.jonathan426@gmail.com>
2023-06-29 22:34:46 -0400

    added google maps with altered controls

--- frontend/src/components/HostSpot/Steps/Step1Location/index.js
+++ frontend/src/components/HostSpot/Steps/Step1Location/index.js
@@ -1,24 +1,46 @@
+import { Loader } from '@googlemaps/js-api-loader';
 import './Step1Location.css';
 
 const Step1Location = ({ location, setLocation }) => {
+    const loader = new Loader({
+        apiKey: process.env.REACT_APP_GOOGLE_PLACES_API,
+        version: 'weekly',
+    });
 
+    loader.load().then(async () => {
+        const { Map } = await window.google.maps.importLibrary("maps");
+
+        const map = new Map(document.getElementById("step-1-map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+            gestureHandling: 'none',
+            zoomControl: false,
+            streetViewControl: false,
+            fullscreenControl: false,
+            mapTypeControl: false,
+            keyboardShortcuts: false,
+            // disableDefaultUI:true,
+        });
+    });
 
     return (
-        <div className='host-step-1-location-container'>
-            <div className='host-step-1-location-container-inner'>
-                <div className='host-step-1-location-top'>
-                    <h1 className='host-step-1-location-header'>Where's your court located?</h1>
-                    <div className='host-step-1-location-subheader'>
-                        Your address is only shared with guests after they've made a reservation.
+        <>
+            <div className='host-step-1-location-container'>
+                <div className='host-step-1-location-container-inner'>
+                    <div className='host-step-1-location-top'>
+                        <h1 className='host-step-1-location-header'>Where's your court located?</h1>
+                        <div className='host-step-1-location-subheader'>
+                            Your address is only shared with guests after they've made a reservation.
+                        </div>
                     </div>
-                </div>
-                <div className='host-step-1-location-bottom'>
-                    <div className='host-step-1-location-main'>
-                        Google Map
+                    <div className='host-step-1-location-bottom'>
+                        <div className='host-step-1-location-main'>
+                            <div id="step-1-map"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
