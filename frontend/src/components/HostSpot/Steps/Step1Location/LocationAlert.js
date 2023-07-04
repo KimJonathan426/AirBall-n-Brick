1c55be83dda1bd27feb45a3e3efd3107d844a73e
Jonathan <kim.jonathan426@gmail.com>
2023-07-04 05:36:53 -0400

    adjusted dom structure and made alert responsive

--- frontend/src/components/HostSpot/Steps/Step1Location/LocationAlert.js
+++ frontend/src/components/HostSpot/Steps/Step1Location/LocationAlert.js
@@ -6,32 +6,36 @@ const LocationAlert = ({ alert, setAlert }) => {
 
 
     return (
-        <div className={alert ? 'invalid-google-location-container-on' : 'invalid-google-location-container-off'}>
-            <div className='invalid-google-location-img-box'>
-                <img className='invalid-google-location-exclamation' src={exclamation} alt='exclamation point' />
-            </div>
-            <div>
-                <div className='invalid-google-location-text-1'>
-                    We don't recognize that address
-                </div>
-                <div className='invalid-google-location-text-2'>
-                    Are you sure that it's correct?
-                </div>
-                <div className='invalid-google-location-main'>
-                    <div className='invalid-google-location-main'>
-                        <button className='invalid-google-location-no'>
-                            No, edit the address
-                        </button>
-                        <button className='invalid-google-location-yes'>
-                            Yes, my address is correct
-                        </button>
+        <div className='invalid-google-location-container'>
+            <div className={alert ? 'invalid-google-location-container-on' : 'invalid-google-location-container-off'}>
+                <div className='invalid-google-location-img-box'>
+                    <div className='invalid-google-location-img-box-inner'>
+                        <img className='invalid-google-location-exclamation' src={exclamation} alt='exclamation point' />
                     </div>
                 </div>
-            </div>
-            <div className='invalid-google-location-exit'>
-                <button className='invalid-google-location-cancel' onClick={() => setAlert(false)}>
-                    <img style={{ width: '16px', height: '16px' }} src={clearX} alt='x'/>
-                </button>
+                <div>
+                    <div className='invalid-google-location-text-1'>
+                        We don't recognize that address
+                    </div>
+                    <div className='invalid-google-location-text-2'>
+                        Are you sure that it's correct?
+                    </div>
+                    <div className='invalid-google-location-main'>
+                        <div className='invalid-google-location-main'>
+                            <button className='invalid-google-location-no'>
+                                No, edit the address
+                            </button>
+                            <button className='invalid-google-location-yes'>
+                                Yes, my address is correct
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='invalid-google-location-exit'>
+                    <button className='invalid-google-location-cancel' onClick={() => setAlert(false)}>
+                        <img style={{ width: '16px', height: '16px' }} src={clearX} alt='x' />
+                    </button>
+                </div>
             </div>
         </div>
     )
