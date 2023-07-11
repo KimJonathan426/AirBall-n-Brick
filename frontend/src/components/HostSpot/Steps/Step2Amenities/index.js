21ed8b74c09633d59597d70521f4ef512fe4cbce
Jonathan <kim.jonathan426@gmail.com>
2023-07-11 10:24:13 -0400

    finished safety amenities and component

--- frontend/src/components/HostSpot/Steps/Step2Amenities/index.js
+++ frontend/src/components/HostSpot/Steps/Step2Amenities/index.js
@@ -18,6 +18,10 @@ import exerciseEquipment from '../../../../images/amenities/exercise-equipment.s
 import bbqGrill from '../../../../images/amenities/bbq-grill.svg';
 import outdoorDining from '../../../../images/amenities/outdoor-dining.svg';
 import playground from '../../../../images/amenities/playground.svg';
+import smokeAlarm from '../../../../images/amenities/smoke-alarm.svg';
+import firstAid from '../../../../images/amenities/first-aid.svg';
+import fireExtinguisher from '../../../../images/amenities/fire-extinguisher.svg';
+import tape from '../../../../images/amenities/tape.svg';
 import './Step2Amenities.css';
 
 const Step2Amenities = ({ amenities, setAmenities }) => {
@@ -33,6 +37,9 @@ const Step2Amenities = ({ amenities, setAmenities }) => {
     ['Dedicated scorers table', scorersTable], ['Referee', referee], ['Pool', pool],
     ['Hot tub', hotTub], ['Shower', shower], ['Exercise equipment', exerciseEquipment],
     ['BBQ grill', bbqGrill], ['Outdoor dining area', outdoorDining], ['Playground', playground]]);
+
+    const [safetyAmenities,] = useState([['Smoke alarm', smokeAlarm], ['First aid kit', firstAid],
+    ['Fire extinguisher', fireExtinguisher], ['Kinesio tape', tape]])
 
     const handleClick = (value) => {
         if (amenities.has(value)) {
@@ -66,10 +73,27 @@ const Step2Amenities = ({ amenities, setAmenities }) => {
                             </div>
                         ))}
                     </div>
-                    <div className='step-2-amenities-standout-header'>Do you have any standout amenities?</div>
+                    <div className='step-2-amenities-list-header standout-amenities' style={{ animationDelay: '700ms' }}>
+                        Do you have any standout amenities?
+                    </div>
                     <div className='step-2-amenities-bottom'>
                         {standoutAmenities.map((option, i) => (
-                            <div key={option[0]} className='step-2-amenities-main' style={{ animationDelay: `${delayed * (1.00 + (.065 - .002 * i) * i)}ms` }}>
+                            <div key={option[0]} className='step-2-amenities-main' style={{ animationDelay: `${delayed * (1.00 + (.065 - .002 * i) * (i + 7))}ms` }}>
+                                <button className={amenities.has(option[0]) ? 'step-2-amenities-btn-checked' : 'step-2-amenities-btn'} onClick={() => handleClick(option[0])}>
+                                    <div style={{ height: '45px' }}>
+                                        <img src={option[1]} alt='icon' />
+                                    </div>
+                                    <div className='step-2-amenities-text'>{option[0]}</div>
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='step-2-amenities-list-header' style={{ animationDelay: '885ms' }}>
+                        Do you have any of these safety items?
+                    </div>
+                    <div className='step-2-amenities-bottom'>
+                        {safetyAmenities.map((option, i) => (
+                            <div key={option[0]} className='step-2-amenities-main' style={{ animationDelay: `${delayed * (1.00 + (.065 - .002 * i) * (i + 12))}ms` }}>
                                 <button className={amenities.has(option[0]) ? 'step-2-amenities-btn-checked' : 'step-2-amenities-btn'} onClick={() => handleClick(option[0])}>
                                     <div style={{ height: '45px' }}>
                                         <img src={option[1]} alt='icon' />
