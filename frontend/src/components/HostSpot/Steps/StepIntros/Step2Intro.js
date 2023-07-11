7a9b984471e35c6a5faff972d44af550fd768f8f
Jonathan <kim.jonathan426@gmail.com>
2023-07-11 08:26:29 -0400

    step 2 intro complete

--- frontend/src/components/HostSpot/Steps/StepIntros/Step2Intro.js
+++ frontend/src/components/HostSpot/Steps/StepIntros/Step2Intro.js
@@ -7,15 +7,15 @@ const Step2Intro = () => {
     return (
         <div className='host-step-intro-container'>
             <div className='host-step-intro-container-inner'>
-                <div className='host-step-intro-left'>
+                <div className='host-step-intro-left step-2-left'>
                     <div className='host-step-intro-left-main-header'>Step 2</div>
                     <h1 className='host-step-intro-left-sub-header'>
-                        Tell us about your&nbsp;court
+                        Make your court stand&nbsp;out
                     </h1>
                     <div className='host-step-intro-left-info'>
-                        In this step, we'll ask you which type of property you have
-                        and if guests will book the entire court or just a portion. Then
-                        let us know the location.
+                        In this step, you'll add some of the amenities your court
+                        offers, plus 5 or more photos. Then, you'll create a title
+                        and description
                     </div>
                 </div>
                 <div className='host-step-intro-right'>
