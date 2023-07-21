f9e1f83fd1ee2eb7cc3b9e171e9786201b6dd4be
Jonathan <kim.jonathan426@gmail.com>
2023-07-21 14:13:36 -0400

    fetch tags from database directly now

--- frontend/src/components/HostSpot/Steps/Step1Tags/index.js
+++ frontend/src/components/HostSpot/Steps/Step1Tags/index.js
@@ -1,28 +1,26 @@
-import { useState } from 'react';
-import indoor from '../../../../images/tags/tag-indoor.png';
-import outdoor from '../../../../images/tags/tag-outdoor.png';
-import stadium from '../../../../images/tags/tag-stadium.png';
-import gym from '../../../../images/tags/tag-gym.png';
-import blacktop from '../../../../images/tags/tag-blacktop.png';
-import wood from '../../../../images/tags/tag-wood.png';
-import concrete from '../../../../images/tags/tag-concrete.png';
-import vinyl from '../../../../images/tags/tag-vinyl.png';
-import double from '../../../../images/tags/tag-double.png';
-import nylon from '../../../../images/tags/tag-nylon.png';
-import chain from '../../../../images/tags/tag-chain.png';
-import netless from '../../../../images/tags/tag-no-net.png';
-import adjustable from '../../../../images/tags/tag-adjustable.png';
-import mini from '../../../../images/tags/tag-mini.png';
+import { useEffect, useState } from 'react';
+import { csrfFetch } from '../../../../store/csrf';
 import '../Steps.css';
 import './Step1Tags.css';
 
 const Step1Tags = ({ tags, setTags }) => {
 
     const [delayed,] = useState(500);
-    const [tagCombos,] = useState([['Indoor', indoor], ['Outdoor', outdoor], ['Stadium', stadium],
-    ['Gym', gym], ['Blacktop', blacktop], ['Wood', wood], ['Concrete', concrete],
-    ['Vinyl', vinyl], ['Double rim', double], ['Nylon net', nylon], ['Chain net', chain],
-    ['No net', netless], ['Adjustable hoop', adjustable], ['Mini hoop', mini]]);
+    const [tagCombos, setTagCombos] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await csrfFetch('/api/tags');
+
+            if (response.ok) {
+                const data = await response.json();
+                setTagCombos(data.tags);
+            };
+        };
+
+        fetchData();
+    }, []);
+
 
     const handleClick = (value) => {
         if (tags.has(value)) {
@@ -44,12 +42,12 @@ const Step1Tags = ({ tags, setTags }) => {
                 </div>
                 <div className='host-step-1-tags-bottom'>
                     {tagCombos.map((option, i) => (
-                        <div key={option[0]} className='host-step-1-tags-main' style={{ animationDelay: `${delayed * (1.00 + (.11 - .0027 * i) * i)}ms` }}>
-                            <button className={tags.has(option[0]) ? 'host-step-1-tags-btn-checked' : 'host-step-1-tags-btn'} onClick={() => handleClick(option[0])}>
+                        <div key={option.id} className='host-step-1-tags-main' style={{ animationDelay: `${delayed * (1.00 + (.11 - .0027 * i) * i)}ms` }}>
+                            <button className={tags.has(option.name) ? 'host-step-1-tags-btn-checked' : 'host-step-1-tags-btn'} onClick={() => handleClick(option.name)}>
                                 <div style={{ height: '45px' }}>
-                                    <img src={option[1]} style={{ width: '35px' }} alt='icon' />
+                                    <img src={option.url} style={{ width: '35px' }} alt='icon' />
                                 </div>
-                                <div className='host-step-1-tags-text'>{option[0]}</div>
+                                <div className='host-step-1-tags-text'>{option.name}</div>
                             </button>
                         </div>
                     ))}
